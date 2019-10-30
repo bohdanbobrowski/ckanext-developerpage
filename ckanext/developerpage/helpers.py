7a4b438e06d584fce6ed64f8ccd3989cfc0c60eb
Bohdan <bohdan.bobrowski@contractors.roche.com>
2019-10-30 14:44:25 +0100

    Add uptime to developerpage

--- ckanext/developerpage/helpers.py
+++ ckanext/developerpage/helpers.py
@@ -3,19 +3,15 @@ import json
 import inspect
 import logging
 import os
-
 from ckan.common import config, is_flask_request, c, request
 from ckan.plugins import toolkit
 from ckan.logic.action.get import status_show
-
 import ckan.model as model
-import ckan.logic as logic
-
 from ckan.common import g, config, _
-
 import platform
 import humanize as hz
-
+import ctypes
+import struct
 
 log = logging.getLogger(__name__)
 
@@ -44,10 +40,38 @@ def load_average_5min():
         }
 
 def get_host_info():
+    intervals = (
+        ('weeks', 604800),  # 60 * 60 * 24 * 7
+        ('days', 86400),  # 60 * 60 * 24
+        ('hours', 3600),  # 60 * 60
+        ('minutes', 60),
+        ('seconds', 1),
+    )
+    def display_time(seconds, granularity=2):
+        result = []
+
+        for name, count in intervals:
+            value = seconds // count
+            if value:
+                seconds -= value * count
+                if value == 1:
+                    name = name.rstrip('s')
+                result.append("{} {}".format(value, name))
+        return ', '.join(result[:granularity])
+    def uptime():
+        libc = ctypes.CDLL('libc.so.6')
+        buf = ctypes.create_string_buffer(4096)  # generous buffer to hold
+        if libc.sysinfo(buf) != 0:
+            print('failed')
+            return -1
+
+        uptime = struct.unpack_from('@l', buf.raw)[0]
+        return display_time(uptime)
     python_platform = {
             'machine_architecture' : platform.machine(),
             'python_version' : platform.python_version(),
-        }
+            'uptime': uptime()
+    }
     memory = memory_info()
     load = load_average_5min()
     python_platform.update(memory)
